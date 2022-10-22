f8f107e6ff2d281a4fcaeba163a8d03f5bf69fc1
MikaBona96 <dmitriygolubapk@gmail.com>
2022-10-22 13:21:56 +0300

    second section done

---
+++ src/components/SecondSection/index.ts
@@ -0,0 +1,6 @@
+import '../../assets/img/2nd-section-img.png';
+export const secondSectionFunction = () => {
+    console.log('>>>secondSectionFunction <<<');
+};
+
+secondSectionFunction();
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 // MarkerGen re-export
+export * from './SecondSection';
 export * from './FirstSection';
 export * from './Header';
 export {};

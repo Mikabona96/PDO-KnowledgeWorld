0c9ef76a61d89ac6316e390d741a0224b766b95d
Mikabona96 <76862391+Mikabona96@users.noreply.github.com>
2022-11-21 09:50:37 +0200

    need to check

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,14 @@ import './elements';
 
 
 let rtl = false;
+
+
+const sections = document.querySelectorAll('section');
+const header = (document.querySelector('header')) as HTMLElement;
+const footer = (document.querySelector('footer')) as HTMLElement;
+sections.forEach((section) => {
+    rtl ? section.classList.add('rtl') : section.classList.remove('rtl');
+});
+
+rtl ? header.classList.add('rtl') : header.classList.remove('rtl');
+rtl ? footer.classList.add('rtl') : footer.classList.remove('rtl');
